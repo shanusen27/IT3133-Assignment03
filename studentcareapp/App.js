6232408c876b9302701b3ff2a6feece843efdf41
shanusen27 <139607388+shanusen27@users.noreply.github.com>
2025-01-06 17:26:30 +0000

    chore: import Login, Home, and Profile components

--- studentcareapp/App.js
+++ studentcareapp/App.js
@@ -4,6 +4,10 @@ import { StyleSheet } from "react-native";
 import { PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Home from "./components/Home";
+
 export default function App() {
   const Stack = createNativeStackNavigator();
 
