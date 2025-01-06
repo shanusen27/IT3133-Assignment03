dbf3d707822090faf863996735ccea126f331125
shanusen27 <139607388+shanusen27@users.noreply.github.com>
2025-01-06 17:25:15 +0000

    style: add container styles to the application

--- studentcareapp/App.js
+++ studentcareapp/App.js
@@ -41,3 +41,13 @@ export default function App() {
     </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
